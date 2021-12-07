92dfb8ee2c26c59a5dad728bbf2a038c2d7498f9
Md. Rased Molla <75846499+rasedprogrammer@users.noreply.github.com>
2021-12-07 21:43:08 +0600

    adding Input value js code for showing h1 tag position input value on there

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const input = document.querySelector("input");
-// input.addEventListener("change", function () {
+const h1 = document.querySelector("h1");
+// input.addEventListener("change", function (e) {
 //   console.log("chalkhlkhalfjdslklksal");
 // });
 
-input.addEventListener("input", function () {
-  console.log("Input Type Here");
+input.addEventListener("input", function (e) {
+  h1.innerText = input.value;
 });
