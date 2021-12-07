ba4a8da1934dbe4972ebb4e22fc4e16f6ccbabad
Md. Rased Molla <75846499+rasedprogrammer@users.noreply.github.com>
2021-12-07 21:30:18 +0600

    adding app.js file and type some js

---
+++ app.js
@@ -0,0 +1,8 @@
+const input = document.querySelector("input");
+// input.addEventListener("change", function () {
+//   console.log("chalkhlkhalfjdslklksal");
+// });
+
+input.addEventListener("input", function () {
+  console.log("Input Type Here");
+});
